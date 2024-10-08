863bcd5a39c89059eb40da5f8c098c15be8bcd0e
gontugithub <gs.alvarez8@gmail.com>
2024-10-08 08:48:31 +0200

    estructura de ficheros spring initializr

---
+++ src/main/java/com/example/ej01Xls/Ej01XlsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ej01Xls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej01XlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej01XlsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ej01Xls/Ej01XlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ej01Xls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej01XlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
